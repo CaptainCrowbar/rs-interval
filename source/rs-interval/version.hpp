94ab1460afb5259216060a4f9a26b6f957f42e5e
Ross Smith <rosssmith@me.com>
2021-12-25 01:32:38 +1100

    Bump version

--- source/rs-interval/version.hpp
+++ source/rs-interval/version.hpp
@@ -4,11 +4,11 @@
 namespace RS::Format {
 
     inline std::array<int, 3> version() noexcept {
-        return {{ 0, 1, 3 }};
+        return {{ 0, 1, 4 }};
     }
 
     inline std::string version_string() {
-        return "0.1.3";
+        return "0.1.4";
     }
 
 }
