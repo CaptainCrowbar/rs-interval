70bf3b22a8dc0eeef75568f408ae6ccdd3f6e3ac
Ross Smith <rosssmith@me.com>
2021-12-26 12:58:17 +1100

    Fix version script

--- source/rs-interval/version.hpp
+++ source/rs-interval/version.hpp
@@ -1,14 +1,16 @@
+#pragma once
+
 #include <string>
 #include <array>
 
 namespace RS::Intervals {
 
     inline std::array<int, 3> version() noexcept {
-        return {{ 0, 1, 17 }};
+        return {{ 0, 1, 18 }};
     }
 
     inline std::string version_string() {
-        return "0.1.17";
+        return "0.1.18";
     }
 
 }
